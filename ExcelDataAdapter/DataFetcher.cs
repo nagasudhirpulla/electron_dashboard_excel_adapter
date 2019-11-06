3c3693eb3a300a54fc83fbae4797044297357707
nagasudhirpulla <nagasudhirpulla@gmail.com>
2019-11-06 22:41:08 +0530

    first commit

---
+++ ExcelDataAdapter/DataFetcher.cs
@@ -0,0 +1,85 @@
+﻿using AdapterUtils;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ExcelDataAdapter
+{
+    public class DataFetcher
+    {
+        public static void FetchAndFlushData(AdapterParams prms)
+        {
+            // get data from excel 
+            // https://stackoverflow.com/questions/37495707/how-to-find-datetime-values-in-an-epplus-excelworksheet?noredirect=1&lq=1
+            // get measurement id from command line arguments. measurement id is formatted as excelFilename|selectedDataCol|selectedTimeCol
+            string measId = prms.MeasId;
+            // get the start and end times
+            DateTime startTime = prms.FromTime;
+            DateTime endTime = prms.ToTime;
+            // get measurement data sources
+            string[] measSegments = measId.Split('|');
+            if (measSegments.Length != 4)
+            {
+                ConsoleUtils.FlushChunks("");
+                return;
+            }
+            string measFilename = measSegments[0];
+            string measSheetname = measSegments[1];
+            string measDataCol = measSegments[2];
+            string measTimeCol = measSegments[3];
+            // check if file exists
+            if (!File.Exists(measFilename))
+            {
+                ConsoleUtils.FlushChunks("");
+                return;
+            }
+            // get excel column names
+            List<string> colNames = new List<string>();
+            FileInfo excelFileInfo = new FileInfo(measFilename);
+            ExcelPackage package = new ExcelPackage(excelFileInfo);
+            ExcelWorksheet sheet = package.Workbook.Worksheets.FirstOrDefault(x => x.Name == measSheetname);
+            if (sheet == null)
+            {
+                ConsoleUtils.FlushChunks("");
+                return;
+            }
+            foreach (var rowCell in sheet.Cells[sheet.Dimension.Start.Row, sheet.Dimension.Start.Column, 1, sheet.Dimension.End.Column])
+            {
+                colNames.Add(rowCell.Text);
+            }
+            // check if desired columns exist
+            int measDataColInd = colNames.FindIndex(x => x == measDataCol) + 1;
+            int measTimeColInd = colNames.FindIndex(x => x == measTimeCol) + 1;
+            if (measDataColInd == 0 || measTimeColInd == 0)
+            {
+                ConsoleUtils.FlushChunks("");
+                return;
+            }
+            // Create output data string
+            string outStr = "";
+            List<object> fetchResult = new List<object>();
+            // https://stackoverflow.com/questions/37495707/how-to-find-datetime-values-in-an-epplus-excelworksheet?noredirect=1&lq=1
+            for (int rowIter = 2; rowIter <= sheet.Dimension.End.Row; rowIter++)
+            {
+                var timeCell = sheet.Cells[rowIter, measTimeColInd];
+                var valCell = sheet.Cells[rowIter, measDataColInd];
+                // check if cell type is time
+                if (timeCell.Value is DateTime valTime)
+                {
+                    // we need to convert data time to utc time before comparing
+                    if ((valTime.ToUniversalTime() >= startTime) && (valTime.ToUniversalTime() <= endTime))
+                    {
+                        fetchResult.Add(TimeUtils.ToMillisSinceUnixEpoch(valTime));
+                        // check if we have a numeric value in the data column
+                        fetchResult.Add((valCell.Value is double) ? valCell.Value : null);
+                    }
+                }
+            }
+            outStr = String.Join(",", fetchResult);
+            // send the output data to console
+            ConsoleUtils.FlushChunks(outStr);
+        }
+    }
+}
---
+++ ExcelDataAdapter/MeasPickerWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using AdapterUtils;
+using Microsoft.Win32;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ExcelDataAdapter
+{
+    /// <summary>
+    /// Interaction logic for MeasPickerWindow.xaml
+    /// </summary>
+    public partial class MeasPickerWindow : Window
+    {
+        // This Configuration data varaible Config_ can be handy here while dealing application secrets or configurations...
+        public MeasPickerWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            ShutdownApp();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            // send selected measurement inforamtion to the console
+            string excelFilename = ExcelFilenameTextBox.Text;
+            string selectedSheet = SheetNamesComboBox.SelectedItem?.ToString();
+            string selectedDataCol = DataColumnNamesComboBox.SelectedItem?.ToString();
+            string selectedTimeCol = TimeColumnNamesComboBox.SelectedItem?.ToString();
+            string resultText = $"{excelFilename}|{selectedSheet}|{selectedDataCol}|{selectedTimeCol}";
+            ConsoleUtils.FlushMeasData(resultText, resultText, resultText);
+
+            // Close the app after sending the data
+            ShutdownApp();
+        }
+
+        private void ShutdownApp()
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void SetColumnOptionsForSelectedSheet()
+        {
+            //check if file exists
+            string fname = ExcelFilenameTextBox.Text;
+            if (!File.Exists(fname))
+            {
+                return;
+            }
+
+            ExcelPackage package = new ExcelPackage(new FileInfo(fname));
+            // populate columns of the selected sheet- https://stackoverflow.com/a/13162400/2746323
+            ExcelWorksheet sheet = package.Workbook.Worksheets.FirstOrDefault(x => x.Name == SheetNamesComboBox.SelectedItem?.ToString());
+            if (sheet == null)
+            {
+                return;
+            }
+
+            List<string> columnNames = new List<string>();
+            foreach (var rowCell in sheet.Cells[sheet.Dimension.Start.Row, sheet.Dimension.Start.Column, 1, sheet.Dimension.End.Column])
+            {
+                columnNames.Add(rowCell.Text);
+            }
+            // changing combobox options
+            DataColumnNamesComboBox.ItemsSource = columnNames;
+            TimeColumnNamesComboBox.ItemsSource = columnNames;
+            if (columnNames.Count > 0)
+            {
+                DataColumnNamesComboBox.SelectedIndex = 0;
+                TimeColumnNamesComboBox.SelectedIndex = 0;
+            }
+        }
+
+        private void OpenExcelFilenameBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // https://www.wpf-tutorial.com/dialogs/the-openfiledialog/
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string fname = openFileDialog.FileName;
+                ExcelFilenameTextBox.Text = fname;
+                //check if file exists
+                if (!File.Exists(fname))
+                {
+                    MessageBox.Show($"could not find a file by name - {fname}");
+                    return;
+                }
+
+                // populate sheet names
+                FileInfo excelFileInfo = new FileInfo(fname);
+                ExcelPackage package = new ExcelPackage(excelFileInfo);
+                List<string> sheetNames = package.Workbook.Worksheets.Select(x => x.Name).ToList();
+                if (sheetNames.Count == 0)
+                {
+                    MessageBox.Show($"No sheets found in - {fname}");
+                    return;
+                }
+                // changing combobox options
+                SheetNamesComboBox.ItemsSource = sheetNames;
+                SheetNamesComboBox.SelectedIndex = 0;
+
+                SetColumnOptionsForSelectedSheet();
+            }
+        }
+
+        private void SheetNamesComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SetColumnOptionsForSelectedSheet();
+        }
+    }
+}
